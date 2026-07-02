6d1cfb15abbeb8e0317f40f7c4855a36b9e30823
w-aaron <w.aaronllan@gmail.com>
2026-07-02 22:57:15 +0800

    Add updateScore showScore playGame with loop functions

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -8,9 +8,6 @@ PSEUDOCODE
 6. Increment score
 */
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice(){
     const choiceNum = Math.floor(Math.random() * 3);
     /* math.random returns number from 0-1 split into 3*/
@@ -24,18 +21,78 @@ function getComputerChoice(){
     else{
         choiceWord = 'Scissors';
     }
-    return choiceWord;
+    return choiceWord.toUpperCase();
     
 }
 
 function getHumanChoice(){
     let choiceWord = prompt('Enter your move: ');
-    return choiceWord;
+    return choiceWord.toUpperCase();
 }
 
 function playGame(human, computer){
     console.log(`Your move is : ${human}`);
     console.log(`Computer move is : ${computer}`);
+
+    if(human === computer){
+        result = 'TIE';
+    }
+    else if(human === 'ROCK'){
+        if(computer === 'PAPER'){
+            result = 'LOSE';
+        }
+        else if(computer === 'SCISSORS'){
+            result = 'WIN';
+        }
+    }
+    else if(human === 'PAPER'){
+        if(computer === 'SCISSORS'){
+            result = 'LOSE';
+        }
+        else if(computer === 'ROCK'){
+            result = 'WIN';
+        }
+    }
+    else if(human === 'SCISSORS'){
+       if(computer === 'ROCK'){
+            result = 'LOSE';
+        }
+        else if(computer === 'PAPER'){
+            result = 'WIN';
+        }
+    }
+
+    updateScore(result);
 }
 
-playGame(getHumanChoice(), getComputerChoice());
+function updateScore(result){
+    if(result === 'WIN'){
+        humanScore += 1;
+    }
+
+    else if(result === 'LOSE'){
+        computerScore += 1;
+    }
+    else if(result === 'TIE'){
+        tieScore += 1;
+    }
+    showScore();
+}
+
+function showScore(){
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+    console.log(`Tie: ${tieScore}`);
+}
+
+
+let result = '';
+let humanScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+for(let i = 0; i < 5; i++){
+    const human = getHumanChoice();
+    const computer = getComputerChoice();
+    playGame(human, computer);
+}
