2230d11436cfc336d81e8c9a170742df9b07c1b3
w-aaron <w.aaronllan@gmail.com>
2026-07-20 17:21:21 +0800

    rps-ui updated index.html and rock_paper_scissors.js

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -8,29 +8,70 @@ PSEUDOCODE
 6. Increment score
 */
 
+
 function getComputerChoice(){
     const choiceNum = Math.floor(Math.random() * 3);
     /* math.random returns number from 0-1 split into 3*/
     let choiceWord = '';
+    const revertBGColor = "black";
+    const updateBGColor = "white";
+    const paper = document.querySelector("#paperComputer");
+    const scissors = document.querySelector("#scissorsComputer");
+    const rock = document.querySelector("#rockComputer");
+
     if(choiceNum === 0){
-        choiceWord = 'Rock';
+        choiceWord = 'rock';
+        rock.style.backgroundColor = updateBGColor;
+        paper.style.backgroundColor = revertBGColor;
+        scissors.style.backgroundColor = revertBGColor;
     }
     else if(choiceNum === 1){
-        choiceWord = 'Paper';
+        choiceWord = 'paper';
+        rock.style.backgroundColor = revertBGColor;
+        paper.style.backgroundColor = updateBGColor;
+        scissors.style.backgroundColor = revertBGColor;
     }
     else{
-        choiceWord = 'Scissors';
+        choiceWord = 'scissors';
+        rock.style.backgroundColor = revertBGColor;
+        paper.style.backgroundColor = revertBGColor;
+        scissors.style.backgroundColor = updateBGColor;
     }
-    return choiceWord.toUpperCase();
-    
+    computer = choiceWord;
 }
 
 function getHumanChoice(){
-    let choiceWord = prompt('Enter your move: ');
-    return choiceWord.toUpperCase();
+    const moves = document.querySelectorAll(".userMove");
+    const score = document.querySelector("#human");
+    moves.forEach((value) => {
+        value.addEventListener("click", () => {
+            const revertBGColor = "black";
+            const paper = document.querySelector("#paper");
+            const scissors = document.querySelector("#scissors");
+            const rock = document.querySelector("#rock");
+
+            value.style.backgroundColor = "white";
+
+            if(value.id === "rock"){
+                paper.style.backgroundColor = revertBGColor;
+                scissors.style.backgroundColor = revertBGColor;
+            }
+            else if(value.id === "paper"){
+                rock.style.backgroundColor = revertBGColor;
+                scissors.style.backgroundColor = revertBGColor;
+            }
+            else if(value.id === "scissors"){
+                paper.style.backgroundColor = revertBGColor;
+                rock.style.backgroundColor = revertBGColor;
+            }
+            human = value.id;
+        })
+    });
 }
 
 function playGame(human, computer){
+    human = human.toUpperCase();
+    computer = computer.toUpperCase();
     console.log(`Your move is : ${human}`);
     console.log(`Computer move is : ${computer}`);
 
@@ -61,8 +102,9 @@ function playGame(human, computer){
             result = 'WIN';
         }
     }
-
     updateScore(result);
+    
+
 }
 
 function updateScore(result){
@@ -76,10 +118,27 @@ function updateScore(result){
     else if(result === 'TIE'){
         tieScore += 1;
     }
-    showScore();
+    showScore(result);
 }
 
-function showScore(){
+function showScore(result){
+    const score = document.querySelector("#score");
+    const addElementHScore = document.createElement("h2");
+    const addElementCScore = document.createElement("h2");
+    const addElementTScore = document.createElement("h2");
+    const resultElement = document.createElement("h2");
+
+    addElementHScore.innerText = `Your score: ${humanScore}`;
+    addElementCScore.innerText = `Computer score: ${computerScore}`;
+    addElementTScore.innerText = `Tie: ${tieScore}`;
+    resultElement.innerText = `You ${result}`;
+    
+    score.textContent = "";
+    score.appendChild(resultElement);
+    score.appendChild(addElementHScore);
+    score.appendChild(addElementCScore);
+    score.appendChild(addElementTScore);
+
     console.log(`Your score: ${humanScore}`);
     console.log(`Computer score: ${computerScore}`);
     console.log(`Tie: ${tieScore}`);
@@ -91,8 +150,25 @@ let humanScore = 0;
 let computerScore = 0;
 let tieScore = 0;
 
-for(let i = 0; i < 5; i++){
-    const human = getHumanChoice();
-    const computer = getComputerChoice();
-    playGame(human, computer);
-}
+// for(let i = 0; i < 5; i++){
+//     const human = getHumanChoice();
+//     const computer = getComputerChoice();
+//     playGame(human, computer);
+// }
+
+let human = "";
+getHumanChoice();
+let computer = "";
+
+const play = document.querySelector(".play");
+play.addEventListener("click", () => 
+    {
+
+        getComputerChoice();
+        playGame(human, computer);
+    }
+);
+
+
+// playGame(human, computer);
+// console.log(score);
